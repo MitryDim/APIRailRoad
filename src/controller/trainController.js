7869b240caa5c8735b3661c87dab66747adb0d94
Dim <44736220+MitryDim@users.noreply.github.com>
2022-12-21 11:23:20 +0100

    .

--- src/controller/trainController.js
+++ src/controller/trainController.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const { default: mongoose } = require('mongoose');
 // Importing train context
 const Train = require("../models/trainModel");
 const Trainstation = require("../models/trainstationModel")
@@ -30,9 +31,20 @@ exports.createTrain = async (req, res) => {
 
 //Update train
 exports.trainUpdate = async (req,res,next) => {
+    let {_id} = req.query;
 
-    const { name } = req.body;
-    const train = await Train.findOne({name})
+    if (_id == undefined)
+        return res.status(400).json({error: "No train id provided"});
+
+    id = mongoose.Types.ObjectId(_id);
+    
+    const train = await Train.findById(id);
+
+    if (train == null)
+        return res.status(404).json({error: "Train Not Found"});
+
+    const {name, start_station, end_station } = req.body;
+
 
     if (train.name != name)
     {
@@ -40,38 +52,34 @@ exports.trainUpdate = async (req,res,next) => {
         if (!isNewName) return res.status(409).send("Name is identical, please enter a new name.");
     }
 
-    if (train.start_station != startStation)
+    if (train.start_station != start_station)
     {
-        const isNewStartStation = await trainstation.isThisNameInUse(startStation);
-        if (!isNewStartStation) return res.status(409).send("Start station not exist, please enter an existing start station.");
+        const isNewStartStation = await Trainstation.isThisNameInUse(start_station);
+        if (isNewStartStation) return res.status(409).send("Start station not exist, please enter an existing start station.");
     }
 
-    if(train.end_station != endStation)
+    if(train.end_station != end_station)
     {
-        const isNewEndStation = await trainstation.isThisNameInUse(endStation);
-        if (!isNewEndStation) return res.status(409).send("End station not exist, please enter an existing end station.")
+        const isNewEndStation = await Trainstation.isThisNameInUse(end_station);
+        if (isNewEndStation) return res.status(409).send("End station not exist, please enter an existing end station.")
     }
 
-    // if(train.time_of_departure != timeOfDeparture)
-    // {
-    //     const isNewTimeOfDeparture = await Train.isThisTimeOfDepartureInUse(timeOfDeparture);
-    //     if (!isNewTimeOfDeparture) return res.status(409).send("Time of departure is identical, please enter a new Time of departure.")
-    // }
 
-    await Train.findOneAndUpdate({name},req.body) //findByIdAndUpdate(req.train._id, req.body)
+    await Train.findByIdAndUpdate(id,req.body) //findByIdAndUpdate(req.train._id, req.body)
     res.status(200).send("updated successfully!");
-
-
 }
 
 
 exports.trainDelete = async (req,res) => {
 
-    //Control of the user's admin role
-    //
-    //
+    let {_id} = req.query;
 
-    await Train.findByIdAndDelete(req.train._id, function (err, docs) {
+    if (_id == undefined)
+        return res.status(400).json({error: "No train id provided"});
+    
+    const id = mongoose.Types.ObjectId(_id);
+        
+    await Train.findByIdAndDelete(id).then(function (err, docs) {
         if (err){
             console.log(err)
         }
--- src/middleware/validation/trainValidation.js
+++ src/middleware/validation/trainValidation.js
@@ -36,35 +36,36 @@ exports.validateInputCreate = (req, res, next) => {
     if (!trainStation.isThisNameInUse(end_station))
         return res.status(400).send("please put an existing end station")
 
+    
+
     next();
 };
 
 exports.validateInputUpdate = (req, res, next) => {
     const schema = Joi.object({
         name: Joi.string()
-            .min(6)
+            .min(3)
             .max(255)
             .required(),
 
         start_station: Joi.string()
-            .min(8)
+            .min(3)
             .max(255)
             .required(),
 
         end_station: Joi.string()
-            .min(8)
+            .min(3)
             .max(255)
             .required(),
 
-        time_of_departure: Joi.date().default(() => moment().format, 'date update')
+
+        time_of_departure: Joi.string()
+                             .regex(/^(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})$/)
     })
     const { error } = schema.validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    if (trainStation.isThisNameInUse(start_station)) return res.start_station(400).send("please put an existing start station")
-
-    if (trainStation.isThisNameInUse(end_station)) return res.start_station(400).send("please put an existing end station")
-
+ 
     next();
 };
 

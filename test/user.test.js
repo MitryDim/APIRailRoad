e56a40668b8d8891c70ac436585fdde448ddd19d
Dim <44736220+MitryDim@users.noreply.github.com>
2023-01-03 22:50:16 +0100

    .

--- test/user.test.js
+++ test/user.test.js
@@ -14,44 +14,74 @@ const URI = process.env.MONGO_URI_TEST || MONGO_URI_TEST
 
 const agent = request.agent(app)
 
+describe('BDD function', () => {
+  
+    before(function (done) {
+        
+       
+mongoose.Promise = global.Promise;
+
+mongoose.connect(URI,
+{
+}
+);
+
+const db = mongoose.connection;
+
+
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+console.log("Connected successfully");
+});
+    });
+})
+
+
+
 // beforeAll(async () => await db.connect());
 // afterEach(async () => await db.clear());
 // afterAll(async () => await db.close());
 
 
-beforeEach(async () => {
-    await mongoose.connect(URI);
-  });
+// beforeEach(async () => {
+//     await mongoose.connect(URI);
+//   });
   
-  /* Closing database connection after each test. */
-  afterEach(async () => {
-    await mongoose.connection.close();
-  });
+//   /* Closing database connection after each test. */
+//   afterEach(async () => {
+//     await mongoose.connection.close();
+//   });
 
 
-  describe('User Integration Test', () => {
-    // make sure app is imported without issues
-    it('Has App Defined', () => {
-        expect(app).toBeDefined();
-    });
+//   describe('User Integration Test', () => {
+//     // make sure app is imported without issues
+//     it('Has App Defined', () => {
+//         expect(app).toBeDefined();
+//     });
 
-    let server;
+//     let server;
 
-    beforeAll(async () => {
-        // Clear Test Data
-        await User.deleteMany({});
-        await Profile.deleteMany({});
-        server = await app.listen(3001);
-    });
+//     beforeAll(async () => {
+//         // Clear Test Data
+//         await User.deleteMany({});
+//         await Profile.deleteMany({});
+//         server = await app.listen(3001);
+//     });
 
-    afterAll(async (done) => {
-        // Clear Test Data
-        await User.deleteMany({});
-        await Profile.deleteMany({});
-        // Close server
-        await server.close(done);
-    });
-});
+//     afterAll(async (done) => {
+//         // Clear Test Data
+//         await User.deleteMany({});
+//         await Profile.deleteMany({});
+//         // Close server
+//         await server.close(done);
+//     });
+// });
+
+
+
+
+
+
 
 // afterEach((done) => {
 //   // Drop the collection and close the connection
